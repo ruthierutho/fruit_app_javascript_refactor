c1fa81b9de51c9878f5403a86f1432ca8f5a4450
Ruth Foulis <ruthlilfoulis@gmail.com>
2021-02-09 13:42:41 +0000

    formatting

--- server/server.js
+++ server/server.js
@@ -10,20 +10,21 @@ app.use(cors());
 
 
 MongoClient.connect('mongodb+srv://ruth:ruth123@cluster0.5eooe.mongodb.net/fruits?retryWrites=true&w=majority')
-.then((client) => {
-    const db = client.db('fruits');
-    const fruitsCollection = db.collection('fruits');
-    const fruitsRouter = createRouter(fruitsCollection);
-    app.use('/api/fruits', fruitsRouter);
-})
-.catch(console.err);
+    .then((client) => {
+        const db = client.db('fruits');
+        const fruitsCollection = db.collection('fruits');
+        const fruitsRouter = createRouter(fruitsCollection);
+        app.use('/api/fruits', fruitsRouter);
+    })
+    .catch(console.err);
 
-if(process.env.NODE_ENV === 'production') {
+
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
 
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 app.listen(3000, function () {
-    console.log(`Listening on port ${ this.address().port }`);
+    console.log(`Listening on port ${this.address().port}`);
 });
